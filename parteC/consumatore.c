697b6b78bc7a3832029196e2ae3bdfe365a5eed3
Stefano Lentini <steflen98@gmail.com>
2019-10-09 23:24:38 +0200

    Inserita lettura caratteri da stdin nel caso di 1 argomento (sequenza caratteri)

--- parteC/consumatore.c
+++ parteC/consumatore.c
@@ -15,6 +15,9 @@ int main(int argc, char* argv[]){
 	char *file_in, read_char, *prefix, buff[MAX_STRING_LENGTH]; 
 	int i, fd, preflen, nread, trovato;
 	
+	prefix =argv[1];
+	preflen=strlen(prefix);
+	
 	if(argc==3){
 		
 		close(0);
@@ -22,33 +25,46 @@ int main(int argc, char* argv[]){
 		fd = open(file_in, O_RDONLY);
 		
 		if (fd!=0){
-			perror("P0: Impossibile aprire il file."); 
+			perror("Apertura file errata.\n"); 
 			exit(2); 
 		}
-	}
+		
+		while(nread = read(fd, &read_char, sizeof(char))){
+			/* un carattere alla volta fino ad EOF*/
 	
-	prefix =argv[1];
-	preflen=strlen(prefix);
-
-	
-	while(nread = read(fd, &read_char, sizeof(char))){
-	/* un carattere alla volta fino ad EOF*/
-	
-		if (nread > 0){
-			trovato=0;
-			for(i=0; i<preflen && !trovato; i++){
-				if(read_char==prefix[i])
-					trovato=1;
+			if (nread > 0){
+				trovato=0;
+				
+				for(i=0; i<preflen && !trovato; i++){
+					if(read_char==prefix[i])
+						trovato=1;
+				}
+				
+				if(!trovato)
+					putchar(read_char);
 			}
-			if(!trovato)
-				putchar(read_char);
+			else
+			{	
+				printf("(PID %d) impossibile leggere dal file %s", getpid(), file_in);
+				perror("Errore!"); close(fd); exit(3);
+			}
+		
 		}
-		else
-		{
-			printf("(PID %d) impossibile leggere dal file %s", getpid(), file_in);
-			perror("Errore!"); close(fd); exit(3);
+		close(fd);
+	}
+	else if(argc==2){
+		
+		while(read_char=getchar()){
+			/* un carattere alla volta fino ad EOF*/
+				trovato=0;
+
+				for(i=0; i<preflen && !trovato; i++){
+					if(read_char==prefix[i])
+						trovato=1;
+				}
+				
+				if(!trovato)
+					putchar(read_char);
 		}
 	}
-	
-close(fd);
 }
